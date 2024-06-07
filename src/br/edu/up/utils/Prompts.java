15767e9ae0fc41f800d6842142905b7aabd552ef
luishf2014 <luishgamer2237@gmail.com>
2024-06-07 20:06:58 -0300

    atualização

---
+++ src/br/edu/up/utils/Prompts.java
@@ -0,0 +1,35 @@
+package br.edu.up.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.catalog.Catalog;
+
+
+public class Prompts {
+
+    List<String> labelCategoria =  Arrays.asList(
+        "Ficção",
+        "Religiosos",
+        "Contos",
+        "Romances",
+        "Didádicos",
+        "Infantis",
+        "Quadrinhos",
+        "Poesia",
+        "Biografia",
+        "Fantasia",
+        "Tecnologia"
+    );
+   
+
+    public String descricaoCategoria(int categoria){
+        
+       return labelCategoria.get(categoria).toString();
+    }
+
+
+   
+}
+
