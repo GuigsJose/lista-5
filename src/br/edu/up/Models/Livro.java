e8968dd814265f59b4aa4a0f5005e44e9cec2c46
luishf2014 <luishgamer2237@gmail.com>
2024-06-10 20:05:08 -0300

    Alteração do menu

--- src/br/edu/up/Models/Livro.java
+++ src/br/edu/up/Models/Livro.java
@@ -1,78 +1,78 @@
-package br.edu.up.Models;
+// package br.edu.up.Models;
 
-public class Livro{
-    /* @athor João: precisa encaixar a Livro para fazer parte de bilbioteca.
-     utilizar ID em vez de código e genero em vez de categoria ( abrange mais coisas do que apenas livros)  
-     - obs: não alterei para não fazer cagada, mas ser quiser eu altero. CMO ELMS*/
+// public class Livro{
+//     /* @athor João: precisa encaixar a Livro para fazer parte de bilbioteca.
+//      utilizar ID em vez de código e genero em vez de categoria ( abrange mais coisas do que apenas livros)  
+//      - obs: não alterei para não fazer cagada, mas ser quiser eu altero. CMO ELMS*/
 
-    public String codigo;
-    public String titulo;
-    public String isbn;
-    public int ano;
-    public Categoria categoria;
+//     public String codigo;
+//     public String titulo;
+//     public String isbn;
+//     public int ano;
+//     public Categoria categoria;
 
-    public Livro(String codigo,String titulo, String isbn, int ano, Categoria categoria){
-        this.codigo = codigo;
-        this.titulo = titulo;
-        this.isbn = isbn;
-        this.ano = ano;
-        this.categoria = categoria;
-    }
+//     public Livro(String codigo,String titulo, String isbn, int ano, Categoria categoria){
+//         this.codigo = codigo;
+//         this.titulo = titulo;
+//         this.isbn = isbn;
+//         this.ano = ano;
+//         this.categoria = categoria;
+//     }
 
     
 
-    @Override
-    public String toString() {
-        return "Livro [codigo=" + codigo + ", titulo=" + titulo + ", isbn=" + isbn + ", ano=" + ano + ", categoria="
-                + categoria + "]";
-    }
+//     @Override
+//     public String toString() {
+//         return "Livro [codigo=" + codigo + ", titulo=" + titulo + ", isbn=" + isbn + ", ano=" + ano + ", categoria="
+//                 + categoria + "]";
+//     }
 
 
 
-    public String toCsv(){
-        return  codigo +";"+ titulo+";" +isbn+";"+ ano+";"+ categoria.getDescricao();
-    }
+//     public String toCsv(){
+//         return  codigo +";"+ titulo+";" +isbn+";"+ ano+";"+ categoria.getDescricao();
+//     }
 
-    public String getCodigo() {
-        return codigo;
-    }
+//     public String getCodigo() {
+//         return codigo;
+//     }
 
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
+//     public void setCodigo(String codigo) {
+//         this.codigo = codigo;
+//     }
 
-    public String getTitulo() {
-        return titulo;
-    }
+//     public String getTitulo() {
+//         return titulo;
+//     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+//     public void setTitulo(String titulo) {
+//         this.titulo = titulo;
+//     }
 
-    public String getIsbn() {
-        return isbn;
-    }
+//     public String getIsbn() {
+//         return isbn;
+//     }
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+//     public void setIsbn(String isbn) {
+//         this.isbn = isbn;
+//     }
 
-    public int getAno() {
-        return ano;
-    }
+//     public int getAno() {
+//         return ano;
+//     }
 
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
+//     public void setAno(int ano) {
+//         this.ano = ano;
+//     }
 
-    public Categoria getCategoria() {
-        return categoria;
-    }
+//     public Categoria getCategoria() {
+//         return categoria;
+//     }
 
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
+//     public void setCategoria(Categoria categoria) {
+//         this.categoria = categoria;
+//     }
     
     
     
-}
+// }
